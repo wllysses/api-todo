6e47880fc6ebfe2fbb4e0b589cdd6b0ea72d38f7
wllysses <wllyssestavares@gmail.com>
2023-11-24 17:49:13 -0300

    fix: error handling middleware

--- src/app.ts
+++ src/app.ts
@@ -3,40 +3,47 @@ import cors from "cors";
 import { router } from "./routes/routes";
 
 class App {
-    private app: Application;
+  private app: Application;
 
-    constructor() {
-        this.app = express();
-        this.config();
-        this.routes();
-    };
-    
-    config() {
-        this.app.use(express.json());
-        this.app.use(express.urlencoded({ extended: true }));
-        this.app.use(cors());
+  constructor() {
+    this.app = express();
+    this.config();
+    this.routes();
+    this.errorHandling();
+  }
 
-        // Error Handling Middleware
-        this.app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
-            if(err instanceof Error) {
-                return res.status(400).json({
-                    message: err.message
-                });
-            };
+  config() {
+    this.app.use(express.json());
+    this.app.use(express.urlencoded({ extended: true }));
+    this.app.use(cors());
+  }
 
-            return res.status(500).json({
-                message: 'Interval Server Error.'
-            });
+  routes() {
+    this.app.use("/api", router);
+  }
+
+  errorHandling() {
+    // Error Handling Middleware
+    this.app.use(
+      (err: Error, req: Request, res: Response, next: NextFunction) => {
+        if (err instanceof Error) {
+          return res.status(400).json({
+            message: err.message,
+          });
+        }
+
+        return res.status(500).json({
+          message: "Interval Server Error.",
         });
-    };
+      }
+    );
+  }
 
-    routes() {
-        this.app.use('/api', router);
-    };
-
-    listen(port: number) {
-        this.app.listen(process.env.PORT || 3000, () => console.log('Server is running on port ' + port));
-    };
+  listen(port: number) {
+    this.app.listen(process.env.PORT || 3000, () =>
+      console.log("Server is running on port " + port)
+    );
+  }
 }
 
 export { App };
